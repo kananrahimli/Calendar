1d5b8e36dac1bcb961cae98f3a059f1e9cb6e076
kananrahimli <kenanrehimli48@gmail.com>
2021-05-01 19:52:10 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <calendar></calendar>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Calendar from './components/Calendar.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+ components:{
+   Calendar
+ }
 }
 </script>
 
